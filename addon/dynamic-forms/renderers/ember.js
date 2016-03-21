f89016653fdd1808f31f46c091b5d9f7140550e2
Todd Jordan <todd.jordan@inin.com>
2016-03-21 01:30:35 -0400

    give the ability to load custom renderers
    load alpaca renderer from its own file
    
    move alpaca renderer under forms dir

---
+++ addon/dynamic-forms/renderers/ember.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Object.extend({
+  render(schema, element) {
+    // iterate fields in schema
+
+    // load component equivs from factory
+
+    // attach to DOM or parent component
+  }
+});
