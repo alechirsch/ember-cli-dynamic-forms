e963d6a5db2012593438d45023ee98a45e1d38cd
Joe DeVries <joedevries@Joes-MBP.fios-router.home>
2016-05-03 10:43:14 -0400

    Add options to exclude handlebars, bootstrap resources, bootstrap views, and loadash.

--- index.js
+++ index.js
@@ -1,32 +1,68 @@
 /* jshint node: true */
 'use strict';
 
+var defaults = {
+	handlebars: true,
+	'bootstrap-css': true,
+	'bootstrap-javascript': true,
+	'bootstrap-fonts': true,
+	'bootstrap-views': true,
+  loadash: true
+};
+
+var getConfigWithDefault = function(property, defaultProperty) {
+	if (property === null || property === undefined) {
+		return defaultProperty;
+	}
+
+	return property;
+};
+
 module.exports = {
   name: 'ember-cli-dynamic-forms',
   included: function (app) {
     this._super.included(app);
-    app.import(app.bowerDirectory + '/handlebars/handlebars.js');
-    app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
-    app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-      destDir: 'fonts'
-    });
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
-      destDir: 'fonts'
-    });
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
-      destDir: 'fonts'
-    });
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
-      destDir: 'fonts'
-    });
-    app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
-      destDir: 'fonts'
-    });
 
-    app.import(app.bowerDirectory + '/alpaca/dist/alpaca/bootstrap/alpaca.js');
-    app.import(app.bowerDirectory + '/alpaca/dist/alpaca/bootstrap/alpaca.css');
-    app.import(app.bowerDirectory + '/lodash/lodash.js');
+    var options = (app && app.options['ember-cli-dynamic-forms']) || {};
+
+    if(getConfigWithDefault(options['handlebars'], defaults['handlebars'])) {
+      app.import(app.bowerDirectory + '/handlebars/handlebars.js');
+    }
+
+    if(getConfigWithDefault(options['bootstrap-javascript'], defaults['bootstrap-javascript'])) {
+      app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
+    }
+
+    if(getConfigWithDefault(options['bootstrap-css'], defaults['bootstrap-css'])) {
+      app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+    }
+
+    if(getConfigWithDefault(options['bootstrap-fonts'], defaults['bootstrap-fonts'])) {
+      app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
+        destDir: 'fonts'
+      });
+      app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
+        destDir: 'fonts'
+      });
+      app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
+        destDir: 'fonts'
+      });
+      app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
+        destDir: 'fonts'
+      });
+      app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
+        destDir: 'fonts'
+      });
+    }
+
+    if(getConfigWithDefault(options['bootstrap-views'], defaults['bootstrap-views'])) {
+      app.import(app.bowerDirectory + '/alpaca/dist/alpaca/bootstrap/alpaca.js');
+      app.import(app.bowerDirectory + '/alpaca/dist/alpaca/bootstrap/alpaca.css');
+    }
+
+    if(getConfigWithDefault(options['loadash'], defaults['loadash'])) {
+      app.import(app.bowerDirectory + '/lodash/lodash.js');
+    }
 
 
   },
